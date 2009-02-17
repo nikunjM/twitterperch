76f386452a5f866864f53c3a41e211643ecbf1e8
ik@webstrong.net <ik@webstrong.net@2ac47584-fce0-11dd-802b-177316e4ac06>
2009-02-17 12:07:50 +0000

    Added first version of the code including a READ_ME file to help users create the database schema and setup their server environment.

---
+++ trunk/inc/config/english.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Twitter Perch - Auto Follow by Keyword on Twitter
+ *
+ * Copyright (c) 2009 Iarfhlaith Kelly (webstrong.ie)
+ * Licensed under GPL (http://www.gnu.org/licenses/gpl.html) license.
+ *
+ * Date: 2009-02-17
+ */
+
+$lang = array(
+	
+ 'index' => array(
+	 				  'success' => 'Great stuff. You\'re all set!'
+	 ,			  'successText' => ''
+	 ,				  'failure' => ''
+	 ,                'invalid' => ''
+	 ,			     'redirect' => ''
+	 ,                'keyword' => 'Please enter the term you want to track in the main field'
+	 ,               'username' => 'You\'ll need to enter your Twitter username.'
+	 ,				 'password' => 'You gotta put your password in too.'
+	)
+	
+);
+
+?>
